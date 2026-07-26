b62c2dc7f199bb833273e5bd925b6cbde46be257
Drackion <64641342+Drackion@users.noreply.github.com>
2026-07-26 15:35:21 +1000

    Update meshes.js

--- kubejs/server_scripts/mods/bbl_strainers/meshes.js
+++ kubejs/server_scripts/mods/bbl_strainers/meshes.js
@@ -5,7 +5,18 @@ ServerEvents.tags('item', allthemods => {
 })
 
 ServerEvents.recipes(allthemods => {
-    allthemods.shaped('kubejs:allthemodium_mesh', [' i ', 'imi', ' i '], {i: 'allthemodium:allthemodium_ingot', m: 'strainers:netherite_mesh'})
+    allthemods.custom({
+      type: "bblcore:shaped_component_copy",
+      category: "misc",
+      copy_components: ["minecraft:enchantments"],
+      key: {
+        A: "allthemodium:allthemodium_ingot",
+        B: "strainers:netherite_mesh",
+      },
+      pattern: [" A ", "ABA", " A "],
+      result: "kubejs:allthemodium_mesh",
+      source: "strainers:netherite_mesh",
+    });
 })
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
